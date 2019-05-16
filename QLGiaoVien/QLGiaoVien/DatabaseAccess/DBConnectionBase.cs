ee3b22e37945a5c0f6c67e08e6309affd3a7b56f
ToanPN <toanphamhsgs@gmail.com>
2019-05-16 19:25:35 +0700

    DBConnectionBase.cs

---
+++ QLGiaoVien/QLGiaoVien/DatabaseAccess/DBConnectionBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLGiaoVien.DatabaseAccess
+{
+    public class DBConnectionBase
+    {
+        private readonly string _connectionString;
+
+        public DBConnectionBase()
+        {
+            _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+
+        protected System.Data.SqlClient.SqlConnection GetConnection()
+        {
+            return new System.Data.SqlClient.SqlConnection(_connectionString);
+        }
+
+    }
+}
